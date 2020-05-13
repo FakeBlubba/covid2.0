3df29b9188656083b672e042beb3faa412ed3a6b
Federico Bianchetti <federicobianchetti@protonmail.ch>
2020-05-13 21:34:07 +0200

    added burgermenu slide feature

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#burger-menu').click(function() {
+    $('.mobile-navbar-menu').addClass('slide-active');
+  });
+  $
+  $('.close-btn-container').click(function() {
+    $('.mobile-navbar-menu').removeClass('slide-active');
+  });
+});
